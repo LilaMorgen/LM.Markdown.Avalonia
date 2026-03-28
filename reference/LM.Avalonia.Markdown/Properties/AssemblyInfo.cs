8dc55cada728a6ad60b8e75899240b33d088749b
LilaMorgen <lilamoegen@lm.com>
2026-03-29 01:25:51 +0800

    移除旧测试代码

--- reference/LM.Avalonia.Markdown/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using Avalonia.Metadata;
-using System.Runtime.CompilerServices;
-
-[assembly: XmlnsDefinition("https://github.com/avaloniaui", "LM.Avalonia.Markdown")]
-[assembly: InternalsVisibleTo("LM.Avalonia.Markdown.Test")]
